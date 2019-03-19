49afc90038efed65e7e11c4134b3fee1d877f045
AhmadMunir <hel000941@gmail.com>
2019-03-19 20:13:36 +0700

    new file:   CI.3.E41170827_P2/tokobuah/application/controllers/Belajar2.php 	modified:   CI.3.E41170827_P2/tokobuah/application/controllers/Welcome.php 	modified:   CI.3.E41170827_P2/tokobuah/application/views/view_belajar.php 	new file:   CI.3.E41170827_P2/tokobuah/application/views/view_belajar2.php

--- CI.3.E41170827_P2/tokobuah/application/views/view_belajar.php
+++ CI.3.E41170827_P2/tokobuah/application/views/view_belajar.php
@@ -16,13 +16,5 @@ menggunakan array : <?php echo $judul ?> ,
 <br />
 <br />
 
-<?php
-echo heading("Helper HTML CodeIgniter | MalasNgoding.com",1);
-echo heading("Helper HTML CodeIgniter | MalasNgoding.com",2);
-echo heading("Helper HTML CodeIgniter | MalasNgoding.com",3);
-echo heading("Helper HTML CodeIgniter | MalasNgoding.com",4);
-echo heading("Helper HTML CodeIgniter | MalasNgoding.com",5);
-echo heading("Helper HTML CodeIgniter | MalasNgoding.com",6);
-?>
 </body>
 </html>
